e30f6055f11cc31827adea0f40db0d385644aa8e
kmastrin <kmastrin@student.alamo.edu>
2023-11-07 19:52:48 -0600

    index

---
+++ app.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  }  
+
+  let i = 5;
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+
+function changeColor() {
+    const element = document.getElementById("myDiv");
+    element.style.backgroundColor = "blue";
+    element.style.color = "white";
+  }
+  
